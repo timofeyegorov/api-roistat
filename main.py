be092322edae3748c60c8c8455890acbdfe47a63
admin <timofeyegorov48@gmail.com>
2021-08-26 14:34:13 +0700

    main commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from pprint import pprint
+from auth_data import api_key, project_id
+
+base_url = 'https://cloud.roistat.com/api/v1/'
+auth_ = f'?key={api_key}&project={project_id}'
+
+analytics_data = 'project/analytics/data'
+dimensions = 'project/analytics/dimensions'
+metrics = 'project/analytics/metrics'
+
+params = {
+  "dimensions": ["marker_level_1"],
+  "metrics": ["marketing_cost", "visitsCost"],
+  "period": {
+    "from":"2021-07-26T00:00:00+0300",
+    "to":"2021-08-25T23:59:59+0300"
+  }
+}
+
+# url = f'{base_url}project/analytics/data{auth_}'
+url = f'{base_url}{analytics_data}{auth_}'
+response = requests.post(url=url, json=params)
+# pprint(response.json())
+channels = response.json()['data'][0]['items']
+# pprint(channels[0])
+with open('channels.txt', 'w', encoding='utf-8') as f:
+    for i in range(len(channels)):
+        f.write(channels[i]['dimensions']['marker_level_1']['title'].replace(u'\xa0', ' ') + ': ' +\
+                str(channels[i]['metrics'][0]['value']) + '\n')
+        pprint(channels[i]['dimensions']['marker_level_1']['title'])
